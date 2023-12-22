90bbf542da6353e90da5935c48d903300287044b
Bruce Wayne <91925396+brucelwayne@users.noreply.github.com>
2023-12-22 08:32:50 +0800

    init

---
+++ database/migrations/2023_12_22_000001_create_searchable_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+
+    protected $table = 'big_searchable';
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create($this->table, function (Blueprint $table) {
+            $table->bigIncrements('id');
+
+            $table->morphs('searchable');
+
+            $table->json('payload');
+
+            $table->timestamps();
+
+            $table->index('created_at');
+            $table->index('updated_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists($this->table);
+    }
+};
---
+++ src/SearchableServiceProvider.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Brucelwayne\SEO;
+
+use Illuminate\Support\ServiceProvider;
+
+class SearchableServiceProvider extends ServiceProvider
+{
+    protected $module_name = 'searchable';
+
+    public function register()
+    {
+
+    }
+
+    function boot()
+    {
+        $this->bootConfigs();
+        $this->bootMigrations();
+    }
+
+    protected function bootConfigs(): void
+    {
+        $this->mergeConfigFrom(
+            __DIR__ . '/../config/searchable.php', $this->module_name
+        );
+    }
+
+    protected function bootMigrations(): void
+    {
+        $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
+    }
+}
